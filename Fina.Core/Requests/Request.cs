a2b6981cf87558f9891e0afe8e2aa62b35e6824d
Mario Puebla Junior <mariopuebla17@gmail.com>
2024-07-31 17:43:57 -0300

    Conhecimentos adquiridos na primeira aula da Jornada Fullstack

---
+++ Fina.Core/Requests/Request.cs
@@ -0,0 +1,11 @@
+﻿namespace Fina.Core.Requests;
+
+public class CreateCategoryRequest : Request
+{
+
+}
+
+public abstract class Request
+{
+    public string UserId { get; set; } = string.Empty;
+}
